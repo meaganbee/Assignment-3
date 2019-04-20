f934f9b3600516aabb9666e82caae6292af6beca
Meagan <meb872@nyu.edu>
2019-04-19 21:36:29 -0400

    Assignment 3
    Add map and marker

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$('#report-button').on('click', function() {
+alert('https://www1.nyc.gov/site/immigrants/about/annual-report.page');
+});
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWVhZ2FuYmVlIiwiYSI6ImNqdWQ5NWNjYTBmZTQzeXJ5YTdoZmo2ZnIifQ.2QCTx5st31pRF3nT1mZDjQ';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/dark-v9',
+center: [-73.907089,40.739844],
+zoom: 10
+
+});
+
+var marker = new mapboxgl.Marker()
+  .setLngLat([-73.907089,40.739844])
+  .addTo(map);
